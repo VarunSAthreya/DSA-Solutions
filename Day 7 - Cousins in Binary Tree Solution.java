af7d338c6c44948c5d871ac3f5f59ed6c95428b7
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-10 02:34:39 +0530

    Update Day 7 - Cousins in Binary Tree Solution.java

--- Day 7 - Cousins in Binary Tree Solution.java
+++ Day 7 - Cousins in Binary Tree Solution.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3322/
+
 /*
 In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
 
@@ -7,21 +9,20 @@ We are given the root of a binary tree with unique values, and the values x and 
 
 Return true if and only if the nodes corresponding to the values x and y are cousins.
 
- 
 
 Example 1:
 
-
 Input: root = [1,2,3,4], x = 4, y = 3
 Output: false
-Example 2:
 
+
+Example 2:
 
 Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
 Output: true
+
+
 Example 3:
-
-
 
 Input: root = [1,2,3,null,4], x = 2, y = 3
 Output: false
