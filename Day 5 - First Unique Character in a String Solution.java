f70a71a9d1ee9cbce4d84508ba5ecd09fc4be224
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-10 02:33:04 +0530

    Update Day 5 - First Unique Character in a String Solution.java

--- Day 5 - First Unique Character in a String Solution.java
+++ Day 5 - First Unique Character in a String Solution.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3320/
+
 /*
 Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
