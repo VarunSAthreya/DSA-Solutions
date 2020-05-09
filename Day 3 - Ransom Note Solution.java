980d844599aa7cce1f62aa863e2e8d9bc32d43f2
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-10 02:31:58 +0530

    Update Day 3 - Ransom Note Solution.java

--- Day 3 - Ransom Note Solution.java
+++ Day 3 - Ransom Note Solution.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3318/
+
 /*
 Given an arbitrary ransom note string and another string containing letters from all the magazines,
 write a function that will return true if the ransom note can be constructed from the magazines ;
