9dcfd593ab219ca90597660e197dd2b67d3d686b
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-11 03:42:18 +0530

    Update Day 10 - Find the Town Judge Solution.java

--- Day 10 - Find the Town Judge Solution.java
+++ Day 10 - Find the Town Judge Solution.java
@@ -45,21 +45,45 @@ Note:
 5. 1 <= trust[i][0], trust[i][1] <= N
 */
 
-//Solution 1
+// Solution 1(faster)
+// O(N2) space
 
 class Solution {
     public int findJudge(int N, int[][] nums) {
-        int[] trusts = new int[N];
-        int[] trusted = new int[N];
+        if(nums.length <  N-1)
+            return -1;
+ 
+        int[] trusts = new int[N+1];
+        int[] trusted = new int[N+1];
         for(int[] x : nums){
-            trusts[x[0]-1]++;
-            trusted[x[1]-1]++;
+            trusts[x[0]]++;
+            trusted[x[1]]++;
         }  
-        for(int i = 0;i<N; i++){
-            if(trusts[i] == 0 && trusted[i] == N-1)
-                return i+1;
+        for(int i = 1;i<=N; i++){
+            if(trusted[i] == N-1 && trusts[i] == 0)
+                return i;
         }
         return -1;
     }
 }
 
+// Solution 2(slower)
+// O(N) space
+
+class Solution {
+    public int findJudge(int N, int[][] nums) {
+        if(nums.length <  N-1)
+            return -1;
+            
+        int[] trusts = new int[N+1];
+        for(int[] x : nums){
+            trusts[x[0]]--;
+            trusts[x[1]]++;
+        }  
+        for(int i = 1;i<=N; i++){
+            if(trusts[i] == N-1)
+                return i;
+        }
+        return -1;
+    }
+}
