f9910693940ce1859e30a86a37b5410297931a75
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-17 03:16:54 +0530

    Update Day 16 - Odd Even Linked List Solution.java

--- Day 16 - Odd Even Linked List Solution.java
+++ Day 16 - Odd Even Linked List Solution.java
@@ -22,3 +22,39 @@ Note:
 1. The relative order inside both the even and odd groups should remain as it was in the input.
 2. The first node is considered odd, the second node even and so on ...
 */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode h_odd = head;
+        ListNode h_even = head.next;
+        ListNode odd = h_odd;
+        ListNode even = h_even;
+        
+        while(even != null){
+            if(even.next != null)
+                odd.next = even.next;
+            else{
+                odd.next = h_even;
+                return h_odd;
+            }
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = h_even;
+        return h_odd;
+    }
+}
