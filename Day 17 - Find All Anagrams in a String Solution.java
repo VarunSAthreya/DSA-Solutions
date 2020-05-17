668f8e53b1906f86bd1a2f59da1d303312e4d35b
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-18 03:16:01 +0530

    Update Day 17 - Find All Anagrams in a String Solution.java

--- Day 17 - Find All Anagrams in a String Solution.java
+++ Day 17 - Find All Anagrams in a String Solution.java
@@ -31,3 +31,37 @@ The substring with start index = 0 is "ab", which is an anagram of "ab".
 The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 */
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int ns = s.length();
+        int np = p.length();
+        int[] sRef = new int[26];
+        int[] pRef = new int[26];
+        List<Integer> results = new ArrayList<>();
+        
+        if(ns < np) {
+            return results; // No solution
+        }
+        
+        // Build the reference array for p
+        for(char ch : p.toCharArray()) {
+            pRef[ch - 'a']++;
+        }
+        
+        // Slide window and compare
+        for(int i = 0; i < ns; i++) {
+            sRef[s.charAt(i) - 'a'] ++; // Add right
+            
+            if(i >= np) {
+                sRef[s.charAt(i - np) - 'a']--; // Remove from left
+            }
+            
+            if(Arrays.equals(sRef, pRef)) {
+                results.add((i - np) + 1);
+            }
+        }
+        
+        return results;
+    }
+}
