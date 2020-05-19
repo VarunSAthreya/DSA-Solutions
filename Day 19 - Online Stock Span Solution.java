cc44a8a6b713c0c4cb201e3e8ed649d762662e0d
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-20 03:07:10 +0530

    Update Day 19 - Online Stock Span Solution.java

--- Day 19 - Online Stock Span Solution.java
+++ Day 19 - Online Stock Span Solution.java
@@ -1,11 +1,14 @@
 // https://leetcode.com/explore/featured/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3334/
 
 /*
-Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+Write a class StockSpanner which collects daily price quotes for some stock,
+and returns the span of that stock's price for the current day.
 
-The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+The span of the stock's price today is defined as the maximum number of consecutive days 
+(starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
 
-For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85],
+then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
 
  
 
@@ -35,3 +38,25 @@ Note:
 4. The total time limit for this problem has been reduced by 75% for C++, and 50% for all other languages.
 */
 
+class StockSpanner {
+ Stack<int[]> s;    
+    public StockSpanner() {
+        s = new Stack<>();
+    }
+    
+   public int next(int price) {
+        int span = 1;
+        while (!s.isEmpty() && price >= s.peek()[0]) { // If the current price is greater than stack peek.
+            span += s.peek()[1];
+            s.pop();
+        }
+        s.push(new int[]{price, span});
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
