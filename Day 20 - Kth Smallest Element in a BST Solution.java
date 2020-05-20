315f5c8cd438c8363ea7af486894e397ee38ac8b
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-21 02:33:26 +0530

    Update Day 20 - Kth Smallest Element in a BST Solution.java

--- Day 20 - Kth Smallest Element in a BST Solution.java
+++ Day 20 - Kth Smallest Element in a BST Solution.java
@@ -31,3 +31,39 @@ What if the BST is modified (insert/delete operations) often and you need to fin
 How would you optimize the kthSmallest routine?
 */
 
+/**
+ * Definition for a binary traee node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result = 0;
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        kthFinder(root);
+        return result;
+    }
+    
+    void kthFinder(TreeNode root){
+        if(root.left != null)
+            kthFinder(root.left);
+        count--;
+        if(count == 0){
+            result = root.val;
+            return;
+        }
+        if(root.right != null)
+            kthFinder(root.right);
+    }
+}
