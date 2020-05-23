a492d51aa6d454c919e86ae0d0926234d2d36570
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-24 00:59:29 +0530

    Update Day 23 - Interval List Intersections Soluton.java

--- Day 23 - Interval List Intersections Soluton.java
+++ Day 23 - Interval List Intersections Soluton.java
@@ -25,3 +25,28 @@ Note:
 3. 0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
 */
 
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int []> result = new ArrayList<>();
+        int i = 0;
+        int j =0;
+        while(i < A.length && j < B.length){
+            int [] a = A[i];
+            int [] b = B[j];
+            if(a[1] < b[0]) 
+                i++;
+            else if(b[1] < a[0])
+                j++;
+            else{//overlap
+                int start = Math.max(a[0], b[0]);
+                int end = Math.min(a[1], b[1]);
+                result.add(new int [] {start, end});
+                if(a[1] < b[1])
+                    i++;
+                else if(a[1] >= b[1])
+                    j++;
+            }
+        }
+        return result.toArray(new int [result.size()][]);
+    }
+}
