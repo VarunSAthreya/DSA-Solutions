3a9841f46c64f39c942e681eb634e26f21630b1a
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-26 02:53:14 +0530

    Update Day 25 - Uncrossed Lines Solution.java

--- Day 25 - Uncrossed Lines Solution.java
+++ Day 25 - Uncrossed Lines Solution.java
@@ -36,3 +36,18 @@ Note:
 2. 1 <= B.length <= 500
 3. 1 <= A[i], B[i] <= 2000
 */
+
+class Solution {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int [][] dp = new int[m+1][n+1];
+        
+        for(int i = 1; i <= m; i++){
+            for(int j =1; j <= n; j++){
+                dp[i][j] = A[i-1] == B[j-1] ? 1 + dp[i-1][j-1] : Math.max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+}
