b6640681ac26cf2c4a25d70f06404e7daa956f3f
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-30 03:49:13 +0530

    Update Day 29 - Course Schedule Solution.java

--- Day 29 - Course Schedule Solution.java
+++ Day 29 - Course Schedule Solution.java
@@ -32,3 +32,36 @@ Constraints:
 2. You may assume that there are no duplicate edges in the input prerequisites.
 3. 1 <= numCourses <= 10^5
 */
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] adj = new ArrayList[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            adj[i] = new ArrayList();
+        }
+        
+        for(int[] pre : prerequisites){
+            adj[pre[0]].add(pre[1]);
+        }
+        
+        int[] visited = new int[numCourses];
+        for(int i = 0; i<numCourses; i++){
+            if(visited[i] == 0 && !dfs(adj ,visited, i))
+                return false;
+        }
+        return true;
+    }
+    
+    private boolean dfs(ArrayList<Integer>[] adj, int[] visited, int v){
+        if(visited[v] == 1)
+            return false;
+        visited[v] =1;
+        for(int ad: adj[v]){
+            if(!dfs(adj, visited, ad))
+                return false;
+        }
+        visited[v] = 2;
+        
+        return true;
+    }
+}
