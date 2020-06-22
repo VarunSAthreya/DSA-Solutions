59e12cde7c5a0c06f116c52f3295bbc43ca28f4e
Varun S Athreya <varun.sathreya@gmail.com>
2020-06-23 00:48:58 +0530

    Update Solution.java

--- Day 22 - Single Number II/Solution.java
+++ Day 22 - Single Number II/Solution.java
@@ -1,3 +1,4 @@
+// with using extra space
 class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -11,4 +12,17 @@ class Solution {
         }
         return -1;
     }
+}
+// without using extra space
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length ; i+=3){
+            if(!(i < nums.length-2 &&nums[i] == nums[i+1] && nums[i] == nums[i+2]))
+                return nums[i];
+        }
+        return 0;
+    }
+        
 }
