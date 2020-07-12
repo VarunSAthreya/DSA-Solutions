1da5f2e69ea2e7f2afb5ad3e960c82bab3b81b59
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-12 23:53:46 +0530

    Updated Soultion

--- July-2020/Day 12 - Reverse Bits/Solution.java
+++ July-2020/Day 12 - Reverse Bits/Solution.java
@@ -1,14 +1,35 @@
 public class Solution {
     // you need treat n as an unsigned value
-   public int reverseBits(int n) {
-    if (n == 0) return 0;
-    
-    int result = 0;
-    for (int i = 0; i < 32; i++) {
-        result <<= 1;
-        if ((n & 1) == 1) result++;
-        n >>= 1;
+    public int reverseBits(int n) {
+        if (n == 0)
+            return 0;
+
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1)
+                result++;
+            n >>= 1;
+        }
+        return result;
     }
-    return result;
 }
+// Another Solution
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int times = 31;
+        int ret = 0;
+
+        while (times >= 0) {
+            int lastDigit = n & 1;
+            if (lastDigit == 1) {
+                ret = ret | (lastDigit << times);
+            }
+            times--;
+            n = n >> 1;
+        }
+        return ret;
+    }
 }
