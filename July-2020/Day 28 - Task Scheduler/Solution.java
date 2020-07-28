f69a82777d0dfd6f339e64f3112485ababdc932d
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-29 00:22:25 +0530

    Day 28

---
+++ July-2020/Day 28 - Task Scheduler/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] frequencies = new int[26];
+        for (char t : tasks) {
+            frequencies[t - 'A']++;
+        }
+        Arrays.sort(frequencies);
+        int maxFreq = frequencies[25];
+        int idleTime = (maxFreq - 1) * n;
+        for (int i = 24; i >= 0; --i) {
+            if (idleTime <= 0) {
+                break;
+            }
+            idleTime -= Math.min(frequencies[i], maxFreq - 1);
+        }
+        idleTime = Math.max(0, idleTime);
+        return idleTime + tasks.length;
+    }
+}
