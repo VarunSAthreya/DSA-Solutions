327382ffec0643b84d834dc2d7c9c4d61a9eaa66
Varun S Athreya <varun.sathreya@gmail.com>
2021-03-06 04:48:19 +0530

    Day 3 update

---
+++ May-2020/Day 3 - Ransom Note/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : magazine.toCharArray()) {
+            if (map.containsKey(ch)) {
+                int count = map.get(ch) + 1;
+                map.put(ch, count);
+            } else
+                map.put(ch, 1);
+        }
+        for (char ch : ransomNote.toCharArray()) {
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+            int count = map.get(ch);
+            if (count == 0)
+                return false;
+            map.put(ch, count - 1);
+        }
+        return true;
+    }
+}
