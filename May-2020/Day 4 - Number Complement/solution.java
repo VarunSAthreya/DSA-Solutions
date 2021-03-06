c6be30f96661a74c53289e48c81cdee8bb8f540d
Varun S Athreya <varun.sathreya@gmail.com>
2021-03-07 04:52:40 +0530

    Update day 4

---
+++ May-2020/Day 4 - Number Complement/solution.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int findComplement(int num) {
+        int cp = num;
+        int sum = 0;
+        while (num > 0) {
+            sum = (sum << 1) + 1;
+            num >>= 1;
+        }
+        return sum - cp;
+
+    }
+}
