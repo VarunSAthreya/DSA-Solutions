0ebde69811635d14bccecfb8fb4a949575be7cef
Varun S Athreya <varun.sathreya@gmail.com>
2021-07-08 01:41:40 +0530

    378. Kth Smallest Element in a Sorted Matrix

---
+++ Topic-Based/Arrays/378. Kth Smallest Element in a Sorted Matrix/README.md
@@ -0,0 +1,29 @@
+# [378. Kth Smallest Element in a Sorted Matrix](https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/)
+
+Given an `n x n` `matrix` where each of the rows and columns are sorted in ascending order, return the <code>k<sup>th</sup></code> _smallest element in the matrix_.
+
+Note that it is the <code>k<sup>th</sup></code> smallest element **in the sorted order**, not the <code>k<sup>th</sup></code> **distinct** element.
+
+**Example 1:**
+
+```
+Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+```
+
+**Example 2:**
+
+```
+Input: matrix = [[-5]], k = 1
+Output: -5
+```
+
+**Constraints:**
+
+-   `n == matrix.length`
+-   `n == matrix[i].length`
+-   `1 <= n <= 300`
+-   <code>-10<sup>9</sup> <= matrix[i][j] <= 10<sup>9</sup></code>
+-   All the rows and columns of matrix are **guaranteed** to be sorted in **non-decreasing order**.
+-   <code>1 <= k <= n<sup>2</sup></code>
---
+++ Topic-Based/Arrays/378. Kth Smallest Element in a Sorted Matrix/Solution.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int[] array = new int[matrix.length * matrix[0].length];
+        int count = 0;
+        // Converting to a single array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                array[count++] = matrix[i][j];
+            }
+        }
+
+        return quickSelect(array, k);
+    }
+
+    public int quickSelect(int[] array, int target) {
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end) {
+            int partitionIndex = partition(array, start, end);
+            if (partitionIndex == target - 1) {
+                return array[target - 1];
+            } else if (partitionIndex < target) {
+                start = partitionIndex + 1;
+            } else {
+                end = partitionIndex - 1;
+            }
+        }
+
+        return array[start];
+    }
+
+    public int partition(int[] array, int start, int end) {
+        int pivot = array[(start + end) / 2];
+
+        while (start < end) {
+            while (array[start] < pivot)
+                start++;
+            while (array[end] > pivot)
+                end--;
+
+            if (array[start] > array[end]) {
+                swap(array, start, end);
+
+            } else {
+                start++;
+            }
+        }
+        return end;
+    }
+}
