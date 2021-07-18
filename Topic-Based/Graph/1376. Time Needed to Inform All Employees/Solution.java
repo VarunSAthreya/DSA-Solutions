71c01a4631ee0a04a74a98fefe05664efff80d0e
Varun S Athreya <varun.sathreya@gmail.com>
2021-07-18 19:21:30 +0530

    1376. Time Needed to Inform All Employees

---
+++ Topic-Based/Graph/1376. Time Needed to Inform All Employees/Solution.java
@@ -0,0 +1,55 @@
+
+// Detailed Solution:
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < manager.length; i++) {
+            graph.add(i, new ArrayList<Integer>());
+        }
+        for (int i = 0; i < manager.length; i++) {
+            if (manager[i] != -1) {
+                ArrayList<Integer> temp = graph.get(manager[i]);
+                temp.add(i);
+                graph.set(manager[i], temp);
+            }
+        }
+
+        return dfs(headID, graph, informTime);
+    }
+
+    private int dfs(int currentId, List<ArrayList<Integer>> graph, int[] informTime) {
+        if (graph.get(currentId).isEmpty()) {
+            return 0;
+        }
+        int max = 0;
+        ArrayList<Integer> subOrdinates = graph.get(currentId);
+        for (int i = 0; i < subOrdinates.size(); i++) {
+            max = Math.max(max, dfs(subOrdinates.get(i), graph, informTime));
+        }
+        return max + informTime[currentId];
+    }
+}
+
+// Best Solution:
+
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dfs(i, manager, informTime));
+        }
+
+        return res;
+    }
+
+    public int dfs(int i, int[] manager, int[] informTime) {
+        if (manager[i] != -1) {
+            informTime[i] += dfs(manager[i], manager, informTime);
+            manager[i] = -1;
+        }
+
+        return informTime[i];
+    }
+}
