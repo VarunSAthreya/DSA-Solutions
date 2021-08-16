7cacbcfe499a15b69d0f6cf50a973302bc8971aa
Varun S Athreya <varun.sathreya@gmail.com>
2021-08-17 04:10:41 +0530

    Added cpp solution for 3. Longest Substring Without Repeating Characters

---
+++ String/3. Longest Substring Without Repeating Characters/Solution-O(2N)Set.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        set<char> st;
+        int n = s.length();
+        int left = 0, right = 0;
+        int count = 0;
+        while (left < n && right < n)
+        {
+            if (st.find(s[right]) == st.end())
+            {
+                st.insert(s[right]);
+                count = max(count, right - left + 1);
+                right++;
+            }
+            else
+            {
+                while (st.find(s[right]) != st.end())
+                {
+                    st.erase(s[left]);
+                    left++;
+                }
+                count = max(count, right - left + 1);
+            }
+        }
+        return count;
+    }
+};
