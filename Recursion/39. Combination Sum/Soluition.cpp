e742d9fbff430b839dbb5d24190959d56c7b7aed
Varun S Athreya <varun.sathreya@gmail.com>
2021-08-29 05:00:05 +0530

    39. Combination Sum

---
+++ Recursion/39. Combination Sum/Soluition.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        vector<vector<int>> res;
+        if (candidates.size() == 0)
+            return res;
+        vector<int> ds;
+        solve(candidates, target, res, ds, 0);
+        return res;
+    }
+    void solve(vector<int> &candidates, int target, vector<vector<int>> &res, vector<int> &ds, int ind)
+    {
+
+        if (ind == candidates.size() || target <= 0)
+        {
+            if (target == 0)
+            {
+                res.push_back(ds);
+            }
+            return;
+        }
+        ds.push_back(candidates[ind]);
+        solve(candidates, target - candidates[ind], res, ds, ind);
+        ds.pop_back();
+        solve(candidates, target, res, ds, ind + 1);
+    }
+};
