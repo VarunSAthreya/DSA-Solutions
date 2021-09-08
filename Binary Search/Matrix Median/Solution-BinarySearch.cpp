fbf889c04eeefcacce24128fb5c82e7347b2c1ac
Varun S Athreya <varun.sathreya@gmail.com>
2021-09-08 16:41:20 +0530

    Added binary search solution

---
+++ Binary Search/Matrix Median/Solution-BinarySearch.cpp
@@ -0,0 +1,44 @@
+int lessThanOrEqual(vector<int> &row, int mid)
+{
+    int l = 0;
+    int h = row.size() - 1;
+    while (l <= h)
+    {
+        int md = l + (h - l) / 2;
+        // > mid
+        if (row[md] <= mid)
+        {
+            l = md + 1;
+        }
+        else
+        {
+            h = md - 1;
+        }
+    }
+    return l;
+}
+
+int Solution::findMedian(vector<vector<int>> &A)
+{
+    int low = 1;
+    int high = 1e9;
+    int n = A.size();
+    int m = A[0].size();
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            cnt += lessThanOrEqual(A[i], mid);
+        }
+
+        if (cnt <= (n * m) / 2)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return low;
+}
