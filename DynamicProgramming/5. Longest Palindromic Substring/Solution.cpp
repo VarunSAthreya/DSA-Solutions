79d0bbfa011eb48bd2081e0bd799dceb17dd641e
Varun S Athreya <varun.sathreya@gmail.com>
2021-10-06 04:32:08 +0530

    Added 0(n solution

--- DynamicProgramming/5. Longest Palindromic Substring/Solution.cpp
+++ DynamicProgramming/5. Longest Palindromic Substring/Solution.cpp
@@ -1,3 +1,4 @@
+// O(2n)
 class Solution
 {
 public:
@@ -26,6 +27,34 @@ public:
                 {
                     if (ans.size() < j - i + 1)
                         ans = s.substr(i, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// O(n)
+
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        int n = s.size();
+        if (n == 0)
+            return "";
+        string ans = "";
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i; j < n; j++)
+            {
+                dp[i][j] = s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j] && j - i + 1 > ans.size())
+                {
+                    ans = s.substr(i, j - i + 1);
                 }
             }
         }
