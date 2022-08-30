cbcb1b7af7b656a9f061892ced52e5484ed9a42f
Varun S Athreya <varun.sathreya@gmail.com>
2022-08-31 03:19:21 +0530

    Decode String

---
+++ String/394. Decode String/Solution.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    string decodeString(string s)
+    {
+        int i = 0;
+        return helper(s, i);
+    }
+
+    string helper(string &s, int &i)
+    {
+        string res = "";
+
+        while (i < s.size() && s[i] != ']')
+        {
+            if (!isdigit(s[i]))
+            {
+                res += s[i++];
+            }
+            else
+            {
+                int n = 0;
+                while (i < s.size() && isdigit(s[i]))
+                    n = (n * 10) + s[i++] - '0';
+
+                i++; // '['
+                string t = helper(s, i);
+                i++; // ']'
+
+                while (n--)
+                    res += t;
+            }
+        }
+
+        return res;
+    }
+};
