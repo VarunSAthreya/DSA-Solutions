5fd0a472dd743a2a36c20cd362625360dd05e8c4
Varun S Athreya <varun.sathreya@gmail.com>
2022-09-08 02:51:30 +0530

    Linked list cycle

---
+++ SDE-Sheet/Linkedlist/8. Linked List Cycle/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        do
+        {
+            if (fast == NULL || fast->next == NULL)
+            {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        } while (slow != fast);
+
+        return true;
+    }
+};
