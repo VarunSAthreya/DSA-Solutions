df33bb9fcaa1fae0a3793f1d87f181aa87f83eba
Varun S Athreya <varun.sathreya@gmail.com>
2022-09-09 02:47:06 +0530

    25. Reverse Nodes in k-Group

---
+++ SDE-Sheet/Linkedlist/9. Reverse Nodes in k-Group/Solution.cpp
@@ -0,0 +1,40 @@
+
+class Solution
+{
+public:
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        if (head == NULL || k == 1)
+            return head;
+
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head; // to indicate new head after reverse
+        ListNode *curr = dummy, *pre = dummy, *nex = dummy;
+        int len = 0; // count the length of the linked list
+        while (curr->next != NULL)
+        {
+            curr = curr->next;
+            len++;
+        }
+
+        while (len >= k) // to leave the last remaining lengths which are less than k
+        {
+            curr = pre->next;
+            nex = curr->next;
+
+            // Runs k-1 times to revese the ll
+            for (int i = 1; i < k; i++)
+            {
+                // Reverse logic
+                curr->next = nex->next;
+                nex->next = pre->next;
+                pre->next = nex;
+                nex = curr->next;
+            }
+            pre = curr;
+            len -= k;
+        }
+        // returns new head
+        return dummy->next;
+    }
+};
