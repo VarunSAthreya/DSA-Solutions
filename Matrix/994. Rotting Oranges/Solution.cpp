d618e38e335616da0853748ed120d3417afb8104
Varun S Athreya <varun.sathreya@gmail.com>
2022-09-12 03:11:37 +0530

    Rotten Oranges - New approach

--- Matrix/994. Rotting Oranges/Solution.cpp
+++ Matrix/994. Rotting Oranges/Solution.cpp
@@ -5,6 +5,7 @@ public:
     {
         if (grid.empty())
             return 0;
+
         int m = grid.size(), n = grid[0].size(), days = 0, tot = 0, cnt = 0;
         queue<pair<int, int>> rotten;
         for (int i = 0; i < m; ++i)
@@ -43,5 +44,61 @@ public:
         }
 
         return tot == cnt ? days : -1;
+    }
+};
+
+//  Different style of BFS
+
+class Solution
+{
+public:
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        int fresh = 1, rotten = 2;
+        int res = 0, freshCnt = 0;
+
+        queue<vector<int>> q;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == fresh)
+                    freshCnt++;
+                else if (grid[i][j] == rotten)
+                    q.push({i, j});
+            }
+        }
+
+        if (freshCnt == 0)
+            return 0;
+
+        while (!q.empty())
+        {
+            int size = q.size();
+            res++;
+
+            while (size--)
+            {
+                auto curr = q.front();
+                q.pop();
+
+                for (auto dir : directions)
+                {
+                    int r = curr[0] + dir[0], c = curr[1] + dir[1];
+                    if (r < n && r >= 0 && c < m && c >= 0 && grid[r][c] == fresh)
+                    {
+                        freshCnt--;
+                        grid[r][c] = rotten;
+                        q.push({r, c});
+                    }
+                }
+            }
+        }
+
+        return freshCnt == 0 ? res - 1 : -1;
     }
 };
