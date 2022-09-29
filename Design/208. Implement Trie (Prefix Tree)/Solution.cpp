0730083598f483168c0fa379cc9be490278bcd53
Varun S Athreya <varun.sathreya@gmail.com>
2022-09-29 19:55:13 +0530

    208. Implement Trie (Prefix Tree)

---
+++ Design/208. Implement Trie (Prefix Tree)/Solution.cpp
@@ -0,0 +1,73 @@
+class Trie
+{
+
+private:
+    class TrieNode
+    {
+    public:
+        map<char, TrieNode *> edges;
+        bool isEnd;
+
+        TrieNode()
+        {
+            isEnd = false;
+        }
+    };
+
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    void insert(string word)
+    {
+        auto curr = root;
+        for (char ch : word)
+        {
+            if (!curr->edges.count(ch))
+            {
+                curr->edges[ch] = new TrieNode;
+            }
+            curr = curr->edges[ch];
+        }
+        curr->isEnd = true;
+    }
+
+    bool search(string word)
+    {
+        auto curr = root;
+        for (char ch : word)
+        {
+            if (!curr->edges.count(ch))
+                return false;
+
+            curr = curr->edges[ch];
+        }
+        return curr->isEnd;
+    }
+
+    bool startsWith(string prefix)
+    {
+        auto curr = root;
+        for (char ch : prefix)
+        {
+            if (!curr->edges.count(ch))
+                return false;
+
+            curr = curr->edges[ch];
+        }
+
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
