e9302bb2c549c41f4d387fc08bdc978ee5f9db92
Varun S Athreya <varun.sathreya@gmail.com>
2022-11-10 02:13:16 +0530

    Remove all Adjacent Duplicate in String

--- Stack/1047. Remove All Adjacent Duplicates In String/Solution-string.cpp
+++ Stack/1047. Remove All Adjacent Duplicates In String/Solution-string.cpp
@@ -14,3 +14,24 @@ public:
         return res;
     }
 };
+
+// Solution 2:
+class Solution
+{
+public:
+    string removeDuplicates(string s)
+    {
+        string res = "";
+        for (int i = 0; i < s.size(); i++)
+        {
+            res.push_back(s[i]);
+            while (res.size() && (res.back() == s[i + 1]))
+            {
+                res.pop_back();
+                i++;
+            }
+        }
+
+        return res;
+    }
+};
